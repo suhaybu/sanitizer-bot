5229c0d30d626ff496d80d689df67876a40784ad
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-01 01:49:16 +0300

    fixed logger

--- src/config/setup.rs
+++ src/config/setup.rs
@@ -2,7 +2,7 @@
 use anyhow::Result;
 use dotenvy::dotenv;
 use tracing::debug;
-use tracing_subscriber::{prelude::*, EnvFilter};
+use tracing_subscriber::EnvFilter;
 
 pub fn init() -> Result<()> {
     setup_logging()?;
@@ -15,13 +15,12 @@ fn setup_logging() -> Result<()> {
     let filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::try_new("info,serenity=warn").expect("Invalid filter"));
 
-    let subscriber =
-        tracing_subscriber::registry().with(tracing_subscriber::fmt::layer().with_filter(filter));
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_target(false)
+        .compact()
+        .init();
 
-    #[cfg(debug_assertions)]
-    let subscriber = subscriber.with(console_subscriber::spawn());
-
-    subscriber.init();
     debug!("Logging initialized");
     Ok(())
 }
