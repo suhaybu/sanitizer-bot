af24832f0b3b366854ef3da1f21ff9b772a212ca
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-01 02:17:54 +0300

    rewrote logic to reflect new structure changes

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,56 @@
-use anyhow::{Error, Result};
-use config::data::Data;
-use config::framework::create_framework;
-use dotenvy::dotenv;
+use anyhow::{Context as _, Error, Result};
 use poise::serenity_prelude as serenity;
+use poise::{Framework, FrameworkOptions};
+use tracing::error;
 
-use tracing::{debug, error};
-use tracing_subscriber::prelude::*;
-use tracing_subscriber::EnvFilter;
+use config::data::Data;
 
 mod commands;
 mod config;
-mod logic;
+mod handlers;
 
 pub type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::try_new("info,serenity=warn").expect("Invalid filter"));
+    if let Err(err) = run().await {
+        error!("Critical error: {:#}", err);
+        error!("Error details: {:#?}", err);
+        std::process::exit(1);
+    }
+    Ok(())
+}
 
-    let subscriber =
-        tracing_subscriber::registry().with(tracing_subscriber::fmt::layer().with_filter(filter));
+async fn run() -> Result<()> {
+    config::setup::init()?;
+    let token = std::env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not found in environment");
 
-    #[cfg(debug_assertions)]
-    let subscriber = subscriber.with(console_subscriber::spawn());
-
-    subscriber.init();
-
-    dotenv().expect(".env file not found");
-
-    let token = std::env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not found");
     let intents = serenity::GatewayIntents::all();
-    let framework = create_framework().expect("Failed to create framework");
+
+    let framework = Framework::builder()
+        .options(FrameworkOptions {
+            event_handler: |ctx, event, framework, data| {
+                Box::pin(handlers::discord::events(ctx, event, framework, data))
+            },
+            commands: commands::get_all(),
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands)
+                    .await
+                    .context("Failed to register commands globally")?;
+                Ok(Data {})
+            })
+        })
+        .build();
 
     let mut client = serenity::ClientBuilder::new(token, intents)
         .framework(framework)
         .await
-        .expect("Error creating client");
+        .context("Failed to create Discord client")?;
 
-    debug!("Bot is starting!");
-    if let Err(error) = client.start().await {
-        error!("Client error: {error:?}");
-        std::process::exit(1);
-    }
+    client.start().await.context("Failed to start client")?;
 
-    Ok(())
-}
-
-// Implemented example handler. Needs to be updated
-async fn event_handler(
-    ctx: &serenity::Context,
-    event: &serenity::FullEvent,
-    _framework: poise::FrameworkContext<'_, Data, Error>,
-    data: &Data,
-) -> Result<(), Error> {
-    match event {
-        serenity::FullEvent::Ready { data_about_bot, .. } => {
-            // TODO
-            debug!("{:?} App is Online", data_about_bot.user.name)
-        }
-        serenity::FullEvent::Message { new_message } => {
-            if new_message.content.to_lowercase().contains("hello")
-                && new_message.author.id != ctx.cache.current_user().id
-            {
-                let author_name = new_message.author.clone().name;
-                println!("{author_name}");
-
-                new_message
-                    .reply(ctx, format!("Hello! #{}", author_name))
-                    .await?;
-            }
-        }
-        _ => {}
-    }
     Ok(())
 }
