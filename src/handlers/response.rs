13696a9e025f036b811da22c12829b6986ca5522
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-09 21:29:06 +0300

    added sanitize_input logic

--- src/handlers/response.rs
+++ src/handlers/response.rs
@@ -1,1 +1,58 @@
+use std::borrow::Cow;
 
+use crate::handlers::{ParsedURL, QuickVidsAPI};
+
+// Responses
+const TWITTER_TEMPLATE: &str = "[@{0} via X (Twitter)](https://fxtwitter.com{1})";
+const INSTAGRAM_API_TEMPLATE: &str = "[@{0} {1} via Instagram]({2})";
+const INSTAGRAM_TEMPLATE: &str = "[{0} via Instagram](https://g.ddinstagram.com/{1}{2})";
+const TIKTOK_TEMPLATE: &str = "[@{0} via TikTok]({1})";
+
+pub async fn sanitize_input(user_input: &str) -> Option<String> {
+    let parsed_url = ParsedURL::new(user_input)?;
+    let api_client = QuickVidsAPI::new();
+
+    let format_post_type = |post_type: &str| -> Cow<'_, str> {
+        match post_type {
+            "p" => Cow::Borrowed("Post"),
+            "reel" => Cow::Borrowed("Reel"),
+            _ => Cow::Owned(post_type.to_string()),
+        }
+    };
+
+    match parsed_url {
+        ParsedURL::Twitter { username, data, .. } => Some(
+            TWITTER_TEMPLATE
+                .replace("{0}", username.as_ref())
+                .replace("{1}", data.as_ref()),
+        ),
+
+        ParsedURL::Instagram {
+            post_type,
+            data,
+            url,
+        } => match api_client.get_response(&url).await {
+            Some(api_response) => Some(
+                INSTAGRAM_API_TEMPLATE
+                    .replace("{0}", &api_response.username.unwrap_or_default())
+                    .replace("{1}", &format_post_type(post_type.as_ref()))
+                    .replace("{2}", &api_response.url),
+            ),
+            None => Some(
+                INSTAGRAM_TEMPLATE
+                    .replace("{0}", &format_post_type(post_type.as_ref()))
+                    .replace("{1}", post_type.as_ref())
+                    .replace("{2}", data.as_ref()),
+            ),
+        },
+
+        ParsedURL::Tiktok { url } => match api_client.get_response(url.as_ref()).await {
+            Some(api_response) => Some(
+                TIKTOK_TEMPLATE
+                    .replace("{0}", &api_response.username.unwrap_or_default())
+                    .replace("{1}", &api_response.url),
+            ),
+            None => None,
+        },
+    }
+}
