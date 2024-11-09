7a70d1698d61b6324fb2a44fbe7807caea55552d
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-09 21:31:43 +0300

    all mods private, only necessary fn are public

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,6 +1,8 @@
-pub mod discord;
+mod discord;
 mod parse_url;
 mod quickvids_api;
 mod response;
 
-pub use parse_url::ParsedURL;
+pub use self::{discord::get_event_handler, response::sanitize_input};
+
+use self::{parse_url::ParsedURL, quickvids_api::QuickVidsAPI};
