8a2a8ff2eabf07e19533f7a1f723a276bd49c841
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-09 18:20:01 +0300

    added message handler logic

--- src/handlers/discord.rs
+++ src/handlers/discord.rs
@@ -1,7 +1,9 @@
-use crate::Data;
 use anyhow::Error;
 use poise::serenity_prelude as serenity;
-use tracing::info;
+use tracing::{debug, info};
+
+use crate::handlers::ParsedURL;
+use crate::Data;
 
 pub async fn get_event_handler(
     ctx: &serenity::Context,
@@ -11,24 +13,56 @@ pub async fn get_event_handler(
 ) -> Result<(), Error> {
     match event {
         serenity::FullEvent::Ready { data_about_bot, .. } => {
-            info!("🤖 {:?} is Online", data_about_bot.user.name)
+            info!("🤖 {} is Online", data_about_bot.user.name.to_string())
         }
         serenity::FullEvent::Message { new_message } => {
-            handle_message(ctx, new_message).await?;
+            handle_messages(ctx, new_message).await?;
         }
         _ => {}
     }
     Ok(())
 }
 
-async fn handle_message(ctx: &serenity::Context, message: &serenity::Message) -> Result<(), Error> {
-    if message.content.to_lowercase().contains("hello")
-        && message.author.id != ctx.cache.current_user().id
-    {
-        let author_name = message.author.name.clone();
-        message
-            .reply(ctx, format!("Hello! #{}", author_name))
-            .await?;
+async fn handle_messages(
+    ctx: &serenity::Context,
+    message: &serenity::Message,
+) -> Result<(), Error> {
+    debug!("Message detected");
+    let input = message.content.trim();
+
+    // Exits if message author is from bot itself
+    if message.author.id == ctx.cache.current_user().id {
+        return Ok(());
     }
+
+    debug!("input = {input}");
+
+    // Check for any supported URLs in the message
+    if let Some(parsed_url) = ParsedURL::new(input) {
+        let response = match parsed_url {
+            ParsedURL::Tiktok { url } => {
+                format!("TikTok link detected (TEST): {url}")
+            }
+            ParsedURL::Instagram {
+                url: _,
+                post_type,
+                data,
+            } => {
+                format!("[{post_type} via Instagram](https://g.ddinstagram.com/{post_type}{data})")
+            }
+            ParsedURL::Twitter {
+                url: _,
+                username,
+                data,
+            } => {
+                format!("[@{username} via X (Twitter)](https://fxtwitter.com/{username}{data})")
+            }
+        };
+
+        debug!("response = {input}");
+        // Reply to the message with the sanitized URL
+        message.reply(ctx, response).await?;
+    }
+
     Ok(())
 }
