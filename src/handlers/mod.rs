0bbe06bcfd12bbf0cce38d4953aaf8e1b17c517b
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-10 00:41:38 +0300

    renamed discord -> discord_events

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,7 +1,7 @@
-mod discord;
+mod discord_events;
 mod response;
 mod user_input;
 
-pub use self::{discord::get_event_handler, response::sanitize_input};
+pub use self::{discord_events::get_event_handler, response::sanitize_input};
 
 use self::user_input::{ParsedURL, QuickVidsAPI};
