145aa7ac9f6d376218e61564b5b1d46184077f23
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-10 18:35:59 +0300

    refactored code, split logic and introduced handle_interaction_response

--- src/handlers/response.rs
+++ src/handlers/response.rs
@@ -11,7 +11,6 @@ pub async fn handle_response(
     ctx: &serenity::Context,
     user_message: &serenity::Message,
     bot_message: &serenity::Message,
-    is_reply: bool,
 ) -> Result<(), Error> {
     // Wait for embeds to appear (up to 10 seconds)
     for _ in 0..10 {
@@ -22,8 +21,9 @@ pub async fn handle_response(
     }
 
     let is_valid_response = check_bot_response(&bot_message);
+
     // Supress embed
-    if is_valid_response && is_reply {
+    if is_valid_response {
         user_message
             .channel_id
             .edit_message(
@@ -32,56 +32,81 @@ pub async fn handle_response(
                 EditMessage::new().suppress_embeds(true),
             )
             .await?;
-    }
 
-    if is_valid_response == false {
+        return Ok(());
+    } else {
         bot_message.delete(ctx).await?;
-        // let error_message = user_message.reply(ctx, CreateEmbed::new().title("Sorry   ꒰ ꒡⌓꒡꒱"))
 
         let error_embed = CreateEmbed::new()
             .title("Sorry   ꒰ ꒡⌓꒡꒱")
             .description("Something went wrong.")
             .color(0xd1001f);
 
-        if is_reply {
-            user_message
-                .delete_reaction_emoji(
-                    ctx,
-                    serenity::ReactionType::Custom {
-                        animated: false,
-                        id: EmojiId::new(1206376642042138724),
-                        name: Some("Sanitized".to_string()),
-                    },
-                )
-                .await?;
+        user_message
+            .delete_reaction_emoji(
+                ctx,
+                serenity::ReactionType::Custom {
+                    animated: false,
+                    id: EmojiId::new(1206376642042138724),
+                    name: Some("Sanitized".to_string()),
+                },
+            )
+            .await?;
 
-            let error_message = user_message
-                .channel_id
-                .send_message(
-                    ctx,
-                    CreateMessage::new()
-                        .reference_message(user_message)
-                        .add_embed(error_embed), // .allowed_mentions(CreateAllowedMentions::new()),
-                )
-                .await?;
+        let error_message = user_message
+            .channel_id
+            .send_message(
+                ctx,
+                CreateMessage::new()
+                    .reference_message(user_message)
+                    .add_embed(error_embed), // .allowed_mentions(CreateAllowedMentions::new()),
+            )
+            .await?;
 
-            sleep(Duration::from_secs(10)).await;
-            error_message.delete(ctx).await?;
-        } else {
-            // Can I make this reply into ephermial
-            user_message
-                .channel_id
-                .send_message(
-                    ctx,
-                    CreateMessage::new()
-                        .reference_message(user_message)
-                        .add_embed(error_embed), // .allowed_mentions(CreateAllowedMentions::new()),
-                )
-                .await?;
+        sleep(Duration::from_secs(10)).await;
+        error_message.delete(ctx).await?;
+
+        return Ok(());
+    }
+}
+
+pub async fn handle_interaction_response(
+    ctx: &serenity::Context,
+    interaction: &CommandInteraction,
+    bot_message: &serenity::Message,
+) -> Result<(), Error> {
+    for _ in 0..10 {
+        sleep(Duration::from_secs(1)).await;
+        if !bot_message.embeds.is_empty() {
+            break;
         }
     }
 
-    todo!()
+    let is_valid_response = check_bot_response(&bot_message);
+
+    if !is_valid_response {
+        // Delete the invalid bot message
+        bot_message.delete(ctx).await?;
+
+        // Create and send ephemeral error message
+        let error_embed = CreateEmbed::new()
+            .title("Sorry   ꒰ ꒡⌓꒡꒱")
+            .description("Something went wrong.")
+            .color(0xd1001f);
+
+        interaction
+            .create_response(
+                ctx,
+                CreateInteractionResponse::Message(
+                    CreateInteractionResponseMessage::new()
+                        .add_embed(error_embed)
+                        .ephemeral(true),
+                ),
+            )
+            .await?;
+    }
+
+    Ok(())
 }
 
 fn check_bot_response(bot_message: &serenity::Message) -> bool {
