6a7015ab39069b60dbf7ea7910930ebb337e0871
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-10 20:04:20 +0300

    added pub fn's: handle_interaction_response, handle_response

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,9 +1,9 @@
-mod cld;
 mod discord_events;
 mod response;
 mod sanitize_input;
 mod user_input;
 
+pub use self::response::{handle_interaction_response, handle_response};
 pub use self::{discord_events::get_event_handler, sanitize_input::sanitize_input};
 
 use self::user_input::{ParsedURL, QuickVidsAPI};
