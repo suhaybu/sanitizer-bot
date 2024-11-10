d97f79b85b02d8acf3c38aac96690a71747c5014
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-10 20:02:43 +0300

    implemented handle_interaction_response logic

--- src/commands/sanitize.rs
+++ src/commands/sanitize.rs
@@ -1,5 +1,6 @@
 use anyhow::Error;
 
+use crate::handlers::handle_interaction_response;
 use crate::handlers::sanitize_input;
 use crate::Context;
 
@@ -12,11 +13,26 @@ pub async fn sanitize(
     ctx: Context<'_>,
     #[description = "Your link goes here"] link: String,
 ) -> Result<(), Error> {
+    // Get initial response ready
     let response = match sanitize_input(&link).await {
         Some(sanitized_url) => sanitized_url,
         None => INVALID_URL_MESSAGE.to_string(),
     };
 
-    ctx.say(response).await?;
+    if let Context::Application(application_ctx) = ctx {
+        // Defer first to get more time
+        application_ctx.defer_response(false).await?;
+
+        let bot_message = ctx.say(response).await?;
+        let bot_message = bot_message.message().await?;
+
+        handle_interaction_response(
+            ctx.serenity_context(),
+            &application_ctx.interaction,
+            &bot_message,
+        )
+        .await?;
+    }
+
     Ok(())
 }
