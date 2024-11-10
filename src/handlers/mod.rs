d0c9ae404dd29d10f8d9702afd4396f033e4eb27
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-10 03:58:04 +0300

    renamed response -> sanitize_input

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,7 +1,7 @@
 mod discord_events;
-mod response;
+mod sanitize_input;
 mod user_input;
 
-pub use self::{discord_events::get_event_handler, response::sanitize_input};
+pub use self::{discord_events::get_event_handler, sanitize_input::sanitize_input};
 
 use self::user_input::{ParsedURL, QuickVidsAPI};
