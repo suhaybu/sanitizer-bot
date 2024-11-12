9738d8e79782f37a9ac78af9505c5959926f507f
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-11-13 01:55:05 +0300

    updated logic

--- src/commands/credits.rs
+++ src/commands/credits.rs
@@ -1,32 +1,21 @@
-use crate::Data;
 use anyhow::Error;
-use poise::serenity_prelude::{
-    CreateEmbed, CreateInteractionResponse, CreateInteractionResponseMessage,
-};
-use poise::ApplicationContext;
+use poise::serenity_prelude as serenity;
+
+use crate::Context;
 
 /// Roll the credits! 🎺
 #[poise::command(slash_command)]
-pub async fn credits(ctx: ApplicationContext<'_, Data, Error>) -> Result<(), Error> {
-    let embed = CreateEmbed::new()
+pub async fn credits(ctx: Context<'_>) -> Result<(), Error> {
+    let embed = serenity::CreateEmbed::new()
         .title("Credits")
         .description("These are all the super cool projects I rely on:\n\
         -  **Twitter**: Thanks to FixTweet's reliable [FxTwitter](https://github.com/FixTweet/FxTwitter) project\n\
         -  **TikTok & Instagram**: Thanks to [QuickVids](https://quickvids.app/) super fast and easy to use API\n\
         -  **Instagram** (Fallback): Powered by the awesome [InstaFix](https://github.com/Wikidepia/InstaFix)");
 
-    // ctx.send(poise::CreateReply::default().embed(embed)).await?;
+    let builder = poise::CreateReply::default().embed(embed).ephemeral(true);
 
-    ctx.interaction
-        .create_response(
-            ctx.serenity_context,
-            CreateInteractionResponse::Message(
-                CreateInteractionResponseMessage::new()
-                    .add_embed(embed)
-                    .ephemeral(true),
-            ),
-        )
-        .await?;
+    ctx.send(builder).await?;
 
     Ok(())
 }
