59d1ab7c2e6d80387477937e857cc189b12765aa
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-12-07 19:45:00 +0300

    removed unused Test function

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,13 +1,11 @@
 use crate::{Data, Error};
 
-mod age;
 mod config;
 mod credits;
 mod sanitize;
 
 pub fn get_all() -> Vec<poise::Command<Data, Error>> {
     vec![
-        age::age(),
         sanitize::sanitize_slash(),
         sanitize::sanitize_menu(),
         config::config(),
