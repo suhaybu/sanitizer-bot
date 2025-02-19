097b8c06dbe3b2ca515d5ef872963b2c7e8ebfa5
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2025-02-19 15:19:21 -0500

    added DatabaseConnection logic

--- src/handlers/db/mod.rs
+++ src/handlers/db/mod.rs
@@ -1,1 +1,55 @@
+use anyhow::{Context as _, Result};
+use mongodb::{
+    bson::Document,
+    options::{ClientOptions, ServerApi, ServerApiVersion},
+};
+use std::sync::Arc;
+use tokio::sync::OnceCell;
+
 pub mod models;
+
+static DB: OnceCell<Arc<DatabaseConnection>> = OnceCell::const_new();
+
+pub struct DatabaseConnection {
+    client: mongodb::Client,
+    db: mongodb::Database,
+}
+
+impl DatabaseConnection {
+    pub async fn new(mongodb_uri: &str, database_name: &str) -> Result<Self> {
+        let mut client_options = ClientOptions::parse(mongodb_uri)
+            .await
+            .context("Failed to parse MongoDB connection sring")?;
+
+        let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
+        client_options.server_api = Some(server_api);
+
+        let client = mongodb::Client::with_options(client_options)
+            .context("Failed to create MongoDB client")?;
+
+        let db = client.database(database_name);
+        Ok(Self { client, db })
+    }
+
+    pub fn get_collection(&self, name: &str) -> mongodb::Collection<Document> {
+        self.db.collection(name)
+    }
+
+    pub async fn init(mongodb_uri: &str, database_name: &str) -> Result<Arc<Self>> {
+        let db = Self::new(mongodb_uri, database_name).await?;
+        Ok(Arc::new(db))
+    }
+
+    pub async fn get() -> Result<Arc<Self>> {
+        DB.get()
+            .context("Database connection not initialized")
+            .map(Arc::clone)
+    }
+
+    pub async fn initialize(mongodb_uri: &str, database_name: &str) -> Result<()> {
+        let db = Self::init(mongodb_uri, database_name).await?;
+        DB.set(db)
+            .map_err(|_| anyhow::anyhow!("Database already initialized"))?;
+        Ok(())
+    }
+}
