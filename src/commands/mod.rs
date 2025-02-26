e5377b21ff8f4e3cc4259cc87e82e9f469c06e38
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2025-02-26 09:56:50 -0500

    removed settings command in favor for a rewrite

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -2,13 +2,11 @@ use crate::{Data, Error};
 
 mod credits;
 mod sanitize;
-mod settings;
 
 pub fn get_all() -> Vec<poise::Command<Data, Error>> {
     vec![
         sanitize::sanitize_slash(),
         sanitize::sanitize_menu(),
-        settings::settings(),
         credits::credits(),
     ]
 }
