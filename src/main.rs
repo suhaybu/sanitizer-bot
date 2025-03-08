0689d976dce5de450d338e8f98caa5fb09e52d3f
Suhayb <75957262+suhaybu@users.noreply.github.com>
2025-03-07 23:30:24 -0500

    updated imports for better readablity

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context as _, Error, Result};
+use poise::FrameworkOptions;
 use poise::serenity_prelude as serenity;
-use poise::{Framework, FrameworkOptions};
 use std::env::var;
 use tracing::error;
 
@@ -29,7 +29,7 @@ async fn run() -> Result<()> {
     let intents =
         serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
 
-    let framework = Framework::builder()
+    let framework = poise::Framework::builder()
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands)
@@ -42,7 +42,7 @@ async fn run() -> Result<()> {
             event_handler: |framework, event| {
                 Box::pin(handlers::get_event_handler(framework, event))
             },
-            commands: commands::get_all(), // Loads all commands from commands/mod.rs -> fn get_all
+            commands: crate::commands::get_all(), // Loads all commands from commands/mod.rs -> fn get_all
             ..Default::default()
         })
         .build();
