04c3309d4b8d051e5f70d86a8dcf3d81e5280d8e
Suhayb <75957262+suhaybu@users.noreply.github.com>
2025-05-29 19:39:26 -0400

    added source code url

--- src/commands/credits.rs
+++ src/commands/credits.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use poise::serenity_prelude as serenity;
+use poise::serenity_prelude::{self as serenity};
 
 use crate::Context;
 
@@ -11,7 +11,8 @@ pub async fn credits(ctx: Context<'_>) -> Result<()> {
         .description("These are all the super cool projects I rely on:\n\
         -  **Twitter**: Thanks to FixTweet's reliable [FxTwitter](https://github.com/FixTweet/FxTwitter) project\n\
         -  **TikTok & Instagram**: Thanks to [QuickVids](https://quickvids.app/) super fast and easy to use API\n\
-        -  **Instagram** (Fallback): Powered by the awesome [InstaFix](https://github.com/Wikidepia/InstaFix)");
+        -  **Instagram** (Fallback): Powered by the awesome [InstaFix](https://github.com/Wikidepia/InstaFix)\n\
+        -# The code for this bot is public sourced on my GitHub [here](https://github.com/suhaybu/sanitizer-bot).");
 
     let builder = poise::CreateReply::default().embed(embed).ephemeral(true);
 
